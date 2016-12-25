959dd9c79a2dece417f4b1ba23095d533f619ff2
Kadir YILMAZ <kadir@kadir.info>
2016-12-25 15:17:43 +0300

    policy gate update

--- src/AclPolicy.php
+++ src/AclPolicy.php
@@ -8,8 +8,21 @@ class AclPolicy
 {
     use HandlesAuthorization;
 
-    public function before($user, $ability)
-    {
-        return $user->checkIfGateAuthorized($ability);
+    public function before($user, $gate){
+    	$count = DB::table('users')
+            ->select('acl_gates.*')
+            ->join('acl_user_groups', 'acl_user_groups.user_id', '=', 'users.id')
+            ->join('acl_groups', 'acl_groups.id', '=', 'acl_user_groups.acl_group_id')
+            ->join('acl_gate_groups', 'acl_gate_groups.acl_group_id', '=', 'acl_groups.id')
+            ->join('acl_gates', 'acl_gates.id', '=', 'acl_gate_groups.acl_gate_id')
+            ->where('users.id', '=', $user->id )
+            ->where(function ($q) use($gate) { //Check if name is null or equal
+                $q  ->whereNull('acl_gates.name')
+                    ->orWhere('acl_gates.name', $gate);
+            })
+            ->count();
+
+        return $count>0;
     }
+
 }
