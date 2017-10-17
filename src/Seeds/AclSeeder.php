784c7ccbf33ab94b5d128dd9542fe6b92dcbb8f1
Kadir YILMAZ <kadir@kadir.info>
2017-10-17 16:09:38 +0300

    acl seeder fix

--- src/Seeds/AclSeeder.php
+++ src/Seeds/AclSeeder.php
@@ -21,18 +21,21 @@ class AclSeeder extends Seeder
             ['user_id' => 1, 'acl_group_id' => 1],
         ]);
 
-        DB::table('acl_gates')->insert([
-            ['id' => 1, 'name' => null], //admin rule
-            ['id' => 2, 'name' => 'upload'],
+        DB::table('acl_user_groups')->insert([
+            ['user_id' => 2, 'acl_group_id' => 2],
         ]);
 
-        DB::table('acl_gate_groups')->insert([
-            ['acl_group_id' => 1, 'acl_gate_id' => 1], //admin
+        DB::table('acl_gates')->insert([
+            ['id' => 1, 'acl_group_id' => 1, 'name' => null], //admin rule
+            ['id' => 2, 'acl_group_id' => 2, 'name' => 'oku'],
+            ['id' => 3, 'acl_group_id' => 2, 'name' => 'upload'],
         ]);
 
         DB::table('acl_controllers')->insert([
             ['id' => 1, 'acl_group_id' => 1, 'controller' => null, 'method' => null], //admin
-            //['id' => 2, 'acl_group_id' => 2, 'controller' => 'Admin\PageAdminController', 'method' => null], //page admin for Editor
+            ['id' => 2, 'acl_group_id' => 1, 'controller' => 'AclController', 'method' => null],
+            ['id' => 3, 'acl_group_id' => 2, 'controller' => 'AclController', 'method' => null],
+            ['id' => 4, 'acl_group_id' => 2, 'controller' => 'HomeController', 'method' => 'index'],
         ]);
     }
 }
